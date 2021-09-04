086493d82528c252c32f7c03fedfc6c5064dd9aa
gabriel passos <gabriel.passos@agibank.com.br>
2021-09-04 18:53:14 -0300

    refact: refactor endpoint to add CORS

--- backend/health-service/src/main/java/com/poc/controller/UserController.java
+++ backend/health-service/src/main/java/com/poc/controller/UserController.java
@@ -14,6 +14,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -57,6 +58,7 @@ public class UserController implements BaseVersion{
         return ResponseEntity.ok(users);
     }
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @Secured(value = {RoleConstant.ADMIN, RoleConstant.USER})
     @GetMapping(value = "/user-auth")
     @ResponseBody
