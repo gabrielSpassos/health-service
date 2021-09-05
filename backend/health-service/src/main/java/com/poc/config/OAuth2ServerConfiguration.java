9406c7e02af4e13ce9997bbddc8a5d72793191f4
gabriel passos <gabriel.passos@agibank.com.br>
2021-09-05 20:24:34 -0300

    chore: update cors configuration

--- backend/health-service/src/main/java/com/poc/config/OAuth2ServerConfiguration.java
+++ backend/health-service/src/main/java/com/poc/config/OAuth2ServerConfiguration.java
@@ -20,6 +20,9 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Res
 import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class OAuth2ServerConfiguration {
@@ -44,6 +47,18 @@ public class OAuth2ServerConfiguration {
                     .and().authorizeRequests()
                     .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                     .anyRequest().fullyAuthenticated();
+        }
+
+        @Bean
+        public CorsConfigurationSource corsConfigurationSource() {
+            CorsConfiguration configuration = new CorsConfiguration();
+            configuration.addAllowedOrigin(CorsConfiguration.ALL);
+            configuration.addAllowedMethod(CorsConfiguration.ALL);
+            configuration.addAllowedHeader(CorsConfiguration.ALL);
+
+            UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+            source.registerCorsConfiguration("/**", configuration);
+            return source;
         }
 
     }
