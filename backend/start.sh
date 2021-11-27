f8bd30f945d98106ee39a4b4dd8a22f2758cd92f
gabriel passos <gabriel.passos@agibank.com.br>
2021-11-27 18:09:55 -0300

    refact: refactor start shell

--- backend/start.sh
+++ backend/start.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-echo "Build Backend Project"
+echo "Build Backend Service Project"
+cd health-service/
 ./gradlew clean build
+cd ..
+
+echo "Build Consumer Project"
+cd health-consumer/
+./gradlew clean build
+cd ..
 
 echo "Start Backend Project"
 docker-compose -f docker-compose.yml build
